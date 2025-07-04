733551c150d45ede66351835e1d7c87b626112b3
regret546 <iregret546@gmail.com>
2025-07-04 15:09:23 +0800

    fix report

--- views/js/reports.js
+++ views/js/reports.js
@@ -3,14 +3,9 @@ LOCAL STORAGE VARIABLE
 =============================================*/
 
 if(localStorage.getItem("captureRange2") != null){
-
-	$("#daterange-btn2 span").html(localStorage.getItem("captureRange2"));
-
-
+    $("#daterange-btn2 span").html(localStorage.getItem("captureRange2"));
 }else{
-
-	$("#daterange-btn2 span").html('<i class="fa fa-calendar"></i> Date Range')
-
+    $("#daterange-btn2 span").html('<i class="fa fa-calendar"></i> Date Range')
 }
 
 /*=============================================
@@ -24,83 +19,60 @@ $('#daterange-btn2').daterangepicker(
       'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
       'Last 7 days' : [moment().subtract(6, 'days'), moment()],
       'Last 30 days': [moment().subtract(29, 'days'), moment()],
-      'this month'  : [moment().startOf('month'), moment().endOf('month')],
-      'Last month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+      'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
     },
     startDate: moment(),
-    endDate  : moment()
+    endDate  : moment(),
+    opens: 'left',
+    autoUpdateInput: true,
+    alwaysShowCalendars: true,
   },
   function (start, end) {
     $('#daterange-btn2 span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
 
     var initialDate = start.format('YYYY-MM-DD');
-
     var finalDate = end.format('YYYY-MM-DD');
-
     var captureRange = $("#daterange-btn2 span").html();
    
-   	localStorage.setItem("captureRange2", captureRange);
-   	console.log("localStorage", localStorage);
+    localStorage.setItem("captureRange2", captureRange);
 
-   	window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
-
+    window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
   }
-
 )
 
 /*=============================================
 CANCEL DATES RANGE
 =============================================*/
 
-$(".daterangepicker.opensright .range_inputs .cancelBtn").on("click", function(){
-
-	localStorage.removeItem("captureRange2");
-	localStorage.clear();
-	window.location = "sales";
+$(".daterangepicker .range_inputs .cancelBtn").on("click", function(){
+    localStorage.removeItem("captureRange2");
+    localStorage.clear();
+    window.location = "reports";
 })
 
 /*=============================================
-CAPTURE TODAY'S BUTTON
+CAPTURE PREDEFINED DATE RANGES
 =============================================*/
 
-$(".daterangepicker.opensright .ranges li").on("click", function(){
+$(".daterangepicker .ranges li").on("click", function(){
+    var selectedRange = $(this).attr("data-range-key");
+    
+    if(selectedRange == "Yesterday"){
+        var yesterday = moment().subtract(1, 'days');
+        var initialDate = yesterday.format('YYYY-MM-DD');
+        var finalDate = yesterday.format('YYYY-MM-DD');
 
-	var todayButton = $(this).attr("data-range-key");
+        localStorage.setItem("captureRange2", "Yesterday");
+        window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
+    }
+    
+    if(selectedRange == "Today"){
+        var today = moment();
+        var initialDate = today.format('YYYY-MM-DD');
+        var finalDate = today.format('YYYY-MM-DD');
 
-	if(todayButton == "Today"){
-
-		var d = new Date();
-		
-		var day = d.getDate();
-		var month= d.getMonth()+1;
-		var year = d.getFullYear();
-
-		if(month < 10){
-
-			var initialDate = year+"-0"+month+"-"+day;
-			var finalDate = year+"-0"+month+"-"+day;
-
-		}else if(day < 10){
-
-			var initialDate = year+"-"+month+"-0"+day;
-			var finalDate = year+"-"+month+"-0"+day;
-
-		}else if(month < 10 && day < 10){
-
-			var initialDate = year+"-0"+month+"-0"+day;
-			var finalDate = year+"-0"+month+"-0"+day;
-
-		}else{
-
-			var initialDate = year+"-"+month+"-"+day;
-	    	var finalDate = year+"-"+month+"-"+day;
-
-		}	
-
-    	localStorage.setItem("captureRange2", "Today");
-
-    	window.location = "index.php?route=sales&initialDate="+initialDate+"&finalDate="+finalDate;
-
-	}
-
+        localStorage.setItem("captureRange2", "Today");
+        window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
+    }
 })
