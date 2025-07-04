2c859e0a7beabc3adf2c31535f933e89f165b825
regret546 <iregret546@gmail.com>
2025-07-04 15:22:04 +0800

    fix daterange issue

--- views/js/reports.js
+++ views/js/reports.js
@@ -22,11 +22,16 @@ $('#daterange-btn2').daterangepicker(
       'This Month'  : [moment().startOf('month'), moment().endOf('month')],
       'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
     },
-    startDate: moment(),
-    endDate  : moment(),
+    startDate: moment().subtract(29, 'days'),
+    endDate: moment(),
     opens: 'left',
     autoUpdateInput: true,
     alwaysShowCalendars: true,
+    showCustomRangeLabel: true,
+    locale: {
+        format: 'YYYY-MM-DD',
+        customRangeLabel: 'Custom Range'
+    }
   },
   function (start, end) {
     $('#daterange-btn2 span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
@@ -63,16 +68,48 @@ $(".daterangepicker .ranges li").on("click", function(){
         var initialDate = yesterday.format('YYYY-MM-DD');
         var finalDate = yesterday.format('YYYY-MM-DD');
 
-        localStorage.setItem("captureRange2", "Yesterday");
-        window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
-    }
-    
-    if(selectedRange == "Today"){
+        // Check if there are sales for yesterday before redirecting
+        $.ajax({
+            url: "ajax/datatable-sales.ajax.php",
+            method: "POST",
+            data: {
+                initialDate: initialDate,
+                finalDate: finalDate
+            },
+            success: function(response) {
+                if(response && response.trim() !== '[]') {
+                    localStorage.setItem("captureRange2", "Yesterday");
+                    window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
+                } else {
+                    // If no data, show message but keep the date picker active
+                    swal({
+                        type: "info",
+                        title: "No Data",
+                        text: "There are no sales records for yesterday",
+                        showConfirmButton: true,
+                        confirmButtonText: "Close"
+                    });
+                }
+            }
+        });
+    } else if(selectedRange == "Today"){
         var today = moment();
         var initialDate = today.format('YYYY-MM-DD');
         var finalDate = today.format('YYYY-MM-DD');
 
         localStorage.setItem("captureRange2", "Today");
+        window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
+    } else if(selectedRange == "Custom Range") {
+        // Do nothing, let the date picker handle it
+        return;
+    } else {
+        // For other predefined ranges
+        var start = $(this).data('daterangepicker').startDate;
+        var end = $(this).data('daterangepicker').endDate;
+        var initialDate = start.format('YYYY-MM-DD');
+        var finalDate = end.format('YYYY-MM-DD');
+
+        localStorage.setItem("captureRange2", selectedRange);
         window.location = "index.php?route=reports&initialDate="+initialDate+"&finalDate="+finalDate;
     }
 })
