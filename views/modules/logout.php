0b38c779c907869b0d97459a86c071e1771f5db9
regret546 <iregret546@gmail.com>
2025-08-14 02:09:24 +0800

    reconfigure .htaccess

--- views/modules/logout.php
+++ views/modules/logout.php
@@ -2,8 +2,17 @@
 
 session_destroy();
 
+// Use absolute URL for better compatibility with hosting environments
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host = $_SERVER['HTTP_HOST'];
+$basePath = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+$redirectUrl = $protocol . $host . $basePath;
+
+// If base path is just '/', remove it to avoid double slash
+if ($basePath === '/') {
+    $redirectUrl = $protocol . $host;
+}
+
 echo '<script>
-
-	window.location = "index.php";
-
+	window.location = "' . $redirectUrl . '";
 </script>';
